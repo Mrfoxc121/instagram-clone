2bf4e722f1a4b0260f80f2dd38dc01df79ff5aa5
Mrfoxc121 <foxc121@yahoo.co.uk>
2022-08-03 15:59:39 +0100

    add logo to left side of header

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    // Left
+
+    <div className="flex justify-between items-center max-w-6xl">
+      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
+          layout="fill"
+          className="object-contain"
+        />
+      </div>
+      <div className="cursor-pointer h-24 w-10 relative  lg:hidden">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg"
+          layout="fill"
+          className="object-contain"
+        />
+      </div>
+      <h1>right side</h1>
+    </div>
+
+    // Middle
+
+    // Right
+  );
 }
