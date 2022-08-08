cd76869d33662d53e2d981a9863600e0b49e7373
Mrfoxc121 <foxc121@yahoo.co.uk>
2022-08-08 15:44:30 +0100

    create the header of and image sections of the post componant

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,21 @@
+import { DotsHorizontalIcon } from "@heroicons/react/solid";
 
-export default function Post ({ id, username, userImg, img, caption }){
-    return (
-    <div>
-        <h1>{username}</h1>
+export default function Post({ id, username, userImg, img, caption }) {
+  return (
+    <div className="bg-white my-7 border rounded-md">
+      {/* post header */}
+
+      <div className="flex items-center p-5">
+        <img
+          className="h-12 w-12 rounded-full object-cover border p-1 mr-3"
+          src={userImg}
+          alt={username}
+        />
+        <p className="font-bold flex-1">{username}</p>
+        <DotsHorizontalIcon className="h-5" />
+      </div>
+      <img className="object-cover w-full" src={img} alt="" />
     </div>
-    )
-}
 
+  );
+}
--- components/Posts.js
+++ components/Posts.js
@@ -7,14 +7,14 @@ export default function Posts() {
             id: "1",
             username: "foxc121",
             userImg: "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1600",
-            img: "https://www.pexels.com/photo/rainbow-in-the-twilight-sky-12513998/",
+            img: "https://images.pexels.com/photos/12641777/pexels-photo-12641777.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
             caption: "Wonderfull scene"
         },
         {
             id: "2",
             username: "Bobfish",
             userImg: "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1600",
-            img: "https://www.pexels.com/photo/smiling-man-sitting-on-leather-couch-laughing-12598765/",
+            img: "https://images.pexels.com/photos/13125805/pexels-photo-13125805.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
             caption: "Enjoying life in the city"
         }
     ]
