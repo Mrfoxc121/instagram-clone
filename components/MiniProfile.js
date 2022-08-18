5bd9b9276ad21567c1d0f85922869819ee4fe067
Mrfoxc121 <foxc121@yahoo.co.uk>
2022-08-18 09:56:46 +0100

    Mofify mini profile and story components to include the session

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import { useSession, signOut } from "next-auth/react"
 
 export default function MiniProfile() {
+  const {data: session} = useSession()
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
-        <img className='h-16 w-16 rounded-full border p-[2px]' src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="user-image" />
+        <img className='h-16 w-16 rounded-full border p-[2px]' src={session?.user.image} alt="user-image" />
         <div className="flex-1 ml-4">
-            <h2 className='font-bold'>Foxc121</h2>
+            <h2 className='font-bold'>{session?.user.username}</h2>
             <h3 className='text-gray-400 text-sm'>Wecome to Instagram</h3>
         </div>
-        <button className='text-blue-400 font-semibold text-sm'>Sign out</button>
+        <button onClick={signOut} className='text-blue-400 font-semibold text-sm'>Sign out</button>
     </div>
   )
 }
