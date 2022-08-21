6439e048656dee33090fdb504aa22c90779104a0
Mrfoxc121 <foxc121@yahoo.co.uk>
2022-08-21 16:17:30 +0100

    create post and add it to firestore and get the dounloadUrl

--- components/UploadModal.js
+++ components/UploadModal.js
@@ -3,36 +3,106 @@ import { modalState } from "../atom/modalAtom";
 import Modal from "react-modal";
 import { CameraIcon } from "@heroicons/react/outline";
 import { useRef, useState } from "react";
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { db, storage } from "../firebase";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
 
 export default function UploadModal() {
   const [open, setOpen] = useRecoilState(modalState);
-  const [selectedFile, setSelectedFile] = useState(null)
-  function addImageTooPost(event){
-    const reader = new FileReader()
-    if(event.target.files[0]){
-        reader.readAsDataURL(event.target.files[0])
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const { data: session } = useSession();
+
+  async function uploadPost() {
+    if (loading) return;
+
+    setLoading(true);
+
+    const docRef = await addDoc(collection(db, "posts"), {
+      caption: captionRef.current.value,
+      username: session.user.username,
+      profileImg: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+
+    const imageRef = ref(storage, `posts/${docRef.id}/image`);
+    await uploadString(imageRef, selectedFile, "data_url").then(
+      async (snapshot) => {
+        const downloadUrl = await getDownloadURL(imageRef);
+        await updateDoc(doc(db, "posts", docRef.id), {
+          image: downloadUrl,
+        });
+      }
+    );
+    setOpen(false);
+    setLoading(false);
+    setSelectedFile(null);
+  }
+  function addImageTooPost(event) {
+    const reader = new FileReader();
+    if (event.target.files[0]) {
+      reader.readAsDataURL(event.target.files[0]);
     }
     reader.onload = (readerEvent) => {
-        setSelectedFile(readerEvent.target.result)
-    }
+      setSelectedFile(readerEvent.target.result);
+    };
   }
-  const filePickerRef = useRef(null)
+  const filePickerRef = useRef(null);
+  const captionRef = useRef(null);
   return (
     <div>
       {open && (
         <Modal
           className="max-w-lg w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md outline-0"
           isOpen={open}
-          onRequestClose={() => {setOpen(false), setSelectedFile(null)}}
+          onRequestClose={() => {
+            setOpen(false), setSelectedFile(null);
+          }}
         >
           <div className="flex flex-col justify-center items-center h-[100%]">
             {selectedFile ? (
-                <img onClick={()=>setSelectedFile(null)} src={selectedFile} alt="" className="w-full max-h-[250px] object-cover cursor-pointer" />
-            ) : (<CameraIcon onClick={() => filePickerRef.current.click()} className="bg-red-200 p-2 rounded-full h-14 border-2 text-red-500 cursor-pointer"/>)}
-            
-            <input onChange={addImageTooPost} type="file" hidden ref={filePickerRef} placeholder="Please leave your caption here..." />
-            <input type="text" maxLength={150} placeholder="Please enter your caption..." className="border-none outline-none m-4 text-center w-full focus:ring-0" />
-            <button disabled className="w-full bg-red-600 text-white p-2 shadow-md hover:brightness-125 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100">Upload Post</button>
+              <img
+                onClick={() => setSelectedFile(null)}
+                src={selectedFile}
+                alt=""
+                className="w-full max-h-[250px] object-cover cursor-pointer"
+              />
+            ) : (
+              <CameraIcon
+                onClick={() => filePickerRef.current.click()}
+                className="bg-red-200 p-2 rounded-full h-14 border-2 text-red-500 cursor-pointer"
+              />
+            )}
+
+            <input
+              onChange={addImageTooPost}
+              type="file"
+              hidden
+              ref={filePickerRef}
+              placeholder="Please leave your caption here..."
+            />
+            <input
+              type="text"
+              maxLength={150}
+              placeholder="Please enter your caption..."
+              ref={captionRef}
+              className="border-none outline-none m-4 text-center w-full focus:ring-0"
+            />
+            <button
+              onClick={uploadPost}
+              disabled={!selectedFile || loading}
+              className="w-full bg-red-600 text-white p-2 shadow-md hover:brightness-125 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100"
+            >
+              Upload Post
+            </button>
           </div>
         </Modal>
       )}
