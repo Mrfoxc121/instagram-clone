c348cf2bb92dd773c7efc210e0212fff8e248b59
Mrfoxc121 <foxc121@yahoo.co.uk>
2022-08-22 09:40:05 +0100

    Get post data from database and show it in the feed section

--- components/Posts.js
+++ components/Posts.js
@@ -1,28 +1,31 @@
-import React from 'react'
-import Post from './Post'
+import { useEffect, useState } from "react";
+import Post from "./Post";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase";
 
 export default function Posts() {
-    const posts = [
-        {
-            id: "1",
-            username: "foxc121",
-            userImg: "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1600",
-            img: "https://images.pexels.com/photos/12641777/pexels-photo-12641777.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-            caption: "Wonderfull scene"
-        },
-        {
-            id: "2",
-            username: "Bobfish",
-            userImg: "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1600",
-            img: "https://images.pexels.com/photos/13125805/pexels-photo-13125805.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-            caption: "Enjoying life in the city"
-        }
-    ]
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, "posts"), orderBy("timestamp", "desc")),
+      (snapshot) => {
+        setPosts(snapshot.docs);
+      }
+    );
+    return unsubscribe;
+  }, [db]); 
   return (
     <div>
-        {posts.map(post => (
-            <Post key={post.id} id={post.id} username={post.username} userImg={post.userImg} img={post.img} caption={post.caption} />
-        ))}
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
+          caption={post.data().caption}
+        />
+      ))}
     </div>
-  )
+  );
 }
